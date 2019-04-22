662f1e7ae4d03de7c59b84815db892fcc0139e25
Ondrej Kohout <ondrej.kohout@gmail.com>
2019-04-22 18:21:07 +0200

    package setup - add black to pre-commit

--- setup.py
+++ setup.py
@@ -5,31 +5,21 @@ from setuptools import find_packages, setup
 
 
 setup(
-    name='nameko-slack',
-    version='0.0.5',
-    description='Nameko extension for interaction with Slack APIs',
-    long_description=open('README.rst').read(),
-    author='Ondrej Kohout',
-    author_email='ondrej.kohout@gmail.com',
-    url='http://github.com/iky/nameko-slack',
-    packages=find_packages(exclude=['test', 'test.*']),
-    install_requires=[
-        "nameko>=2.7.0",
-        "slackclient>=1.0.4",
-    ],
+    name="nameko-slack",
+    version="0.0.5",
+    description="Nameko extension for interaction with Slack APIs",
+    long_description=open("README.rst").read(),
+    author="Ondrej Kohout",
+    author_email="ondrej.kohout@gmail.com",
+    url="http://github.com/iky/nameko-slack",
+    packages=find_packages(exclude=["test", "test.*"]),
+    install_requires=["nameko>=2.7.0", "slackclient>=1.0.4"],
     extras_require={
-        'dev': [
-            "coverage",
-            "flake8",
-            "isort",
-            "pre-commit",
-            "pylint",
-            "pytest",
-        ]
+        "dev": ["coverage", "flake8", "isort", "pre-commit", "pylint", "pytest"]
     },
     dependency_links=[],
     zip_safe=True,
-    license='Apache License, Version 2.0',
+    license="Apache License, Version 2.0",
     classifiers=[
         "Programming Language :: Python",
         "Operating System :: MacOS :: MacOS X",
@@ -43,5 +33,5 @@ setup(
         "Topic :: Internet",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Intended Audience :: Developers",
-    ]
+    ],
 )
